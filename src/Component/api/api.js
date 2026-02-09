b76cea5977809b51b808678ff80901fa951b9c89
PrathamSachan91 <prathamsachan6886@gmail.com>
2026-02-09 17:23:26 +0530

    deploy

--- src/Component/api/api.js
+++ src/Component/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https:talkify-backend-q62n.onrender.com/api",
+  baseURL: "https://talkify-backend-q62n.onrender.com/api",
   withCredentials: true, // sends cookies automatically
   headers: {
     "Content-Type": "application/json",
