b7db11666f00fe64f5a7c732689205dd3b42052f
PrathamSachan91 <prathamsachan6886@gmail.com>
2026-02-09 16:30:10 +0530

    deploy

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,15 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:3001", {
+//   autoConnect: false,
+//   withCredentials: true,
+// });
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
 });
