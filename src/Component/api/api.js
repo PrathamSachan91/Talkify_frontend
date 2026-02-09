232b587da7c10e9807f80a664a700e7348ecbb0d
PrathamSachan91 <prathamsachan6886@gmail.com>
2026-02-09 13:36:22 +0530

    deploy

--- src/Component/api/api.js
+++ src/Component/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true, // sends cookies automatically
   headers: {
     "Content-Type": "application/json",
