e825383f84bfd0368efd9b940e4769840ba1423a
PrathamSachan91 <prathamsachan6886@gmail.com>
2026-02-18 18:06:50 +0530

    Host

--- src/Component/api/api.js
+++ src/Component/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
-// baseURL: "https://talkify-backend-q62n.onrender.com/api",
+  // baseURL: "http://localhost:3001/api",
+baseURL: "https://talkify-backend-q62n.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
